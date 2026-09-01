3d2ece40b8680f0dc705a25b82767a6855efab07
Naufal Dinta <naufaldintaa@gmail.com>
2026-09-01 14:37:10 +0700

    change pos to outlet

--- src/pages/report/pos/productItem.tsx
+++ src/pages/report/pos/productItem.tsx
@@ -47,7 +47,7 @@ export default function POSProductItemPage() {
     useOutletType();
 
   useEffect(() => {
-    getOutletType({ search: "POS" });
+    getOutletType({ search: "Outlet" });
   }, []);
 
   useEffect(() => {
